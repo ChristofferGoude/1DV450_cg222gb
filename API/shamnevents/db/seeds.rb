237b380b1800861c1fa77057d42f274576b16994
ChristofferGoude <christoffer.goude@gmail.com>
2015-06-12 19:46:30 +0200

    Added seedfile and Postman Test collection via json

--- API/shamnevents/db/seeds.rb
+++ API/shamnevents/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Creator.delete_all
+creator1 = Creator.create(name: "Klas Testsson", password: "klaspass")
+creator2 = Creator.create(name: "Glenn Danzig", password: "superpassword")
+creator3 = Creator.create(name: "Mr. T", password: "ipitythepassword")
+
+Tag.delete_all
+tag1 = Tag.create(name: "Test Tag")
+tag2 = Tag.create(name: "Best Tag")
+tag3 = Tag.create(name: "Next Tag")
+
+Event.delete_all
+Event.create(creator_id: creator1.id, name: "Concert", latitude: 61.3055762, longitude: 17.0628102, tags: [tag1])
+Event.create(creator_id: creator2.id, name: "Theater show", latitude: 61.7273909, longitude: 17.10740099, tags: [tag1, tag2, tag3])
+Event.create(creator_id: creator3.id, name: "Car race", latitude: 57.70887, longitude: 11.974559999, tags: [tag2, tag3])
